0fa70c11430214b8263a65af64f845c90e113644
madziaZSLgda <113981700+madziaZSLgda@users.noreply.github.com>
2022-09-30 13:36:40 +0200

    Add files via upload

---
+++ madzia 1d/c++/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=4;
+    cout << "Liczba a wynosi " <<a<< endl;
+    cout << "Powiekszona o 2 wynosi" <<a+2<< endl;
+    cout<<"Podaj zmienna b"<<endl;
+    cin>>"W zmiennej b jest "<<b<<endl;
+    int suma=a+b;
+    cout<<"Suma"<<endl;
+    return 0;
+}
